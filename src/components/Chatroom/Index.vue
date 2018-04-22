6349ebca495b06c96b324b49c42a3e3a5a1d07d1
zhangxianlei <863837949@qq.com>
2018-04-22 17:27:13 +0800

    吟诗作对初始化

--- src/components/Chatroom/Index.vue
+++ src/components/Chatroom/Index.vue
@@ -6,14 +6,53 @@
             </router-link>
             <mt-button icon="more" slot="right"></mt-button>
         </mt-header>
+        <div class="chat-main">
+            <div class="chat-panel">
+                <div :key="key" v-for="(item, key) in msgPool">
+                    {{ item.username }}: {{ item.msg }}
+                </div>
+            </div>
+
+            <div class="input-msg">
+                <textarea v-model="msg" placeholder="骚友请题诗..."></textarea>
+                <mt-button class="chat-send" type="primary" @click="sendMsg">发送</mt-button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import io from 'socket.io-client'
 export default {
     name: 'Chatroom',
+    created () {
+        this.socket = io.connect('ws://127.0.0.1:3000')
+        this.socket.emit('add user', {name: 'bob'})
+        // socket.emit('add user', {name: 'bob'})
+        // socket.emit('new message', {msg: 'hello iam fine'})
+
+        var self = this
+        this.socket.on('new message', function (data) {
+            self.msgPool.push({
+                username: data.username.name,
+                msg: data.message.msg
+            })
+        })
+    },
+    methods: {
+        sendMsg () {
+            this.msgPool.push({
+                username: 'bob',
+                msg: this.msg
+            })
+            this.socket.emit('new message', {msg: this.msg})
+        }
+    },
     data () {
         return {
+            msg: '',
+            msgPool: [],
+            socket: null
         }
     },
     computed: {
